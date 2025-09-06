e26393acd36e34260a5b95aae7b820eb87a70192
ScottCatton <78232367+MapleSerup@users.noreply.github.com>
2025-09-06 10:08:35 -0400

    deleted some old comments, need to add delete before submitting

--- src/app/services/tasks-service.ts
+++ src/app/services/tasks-service.ts
@@ -33,25 +33,9 @@ export class TasksService {
       Accept: 'application/json',
     });
     const options = { headers: headers };
-    // console.log(JSON.stringify(form));
-    // let taskToAdd: ITask = form.value; // this is undefined, trying to go form -> task
-    // let taskToAdd2: ITask = {
-    //   title: form.title,
-    //   description: form.description,
-    //   date: form.date,
-    //   time: form.time,
-    //   priority_level: form.priority_level,
-    //   category: form.category,
-    //   progress_level: form.progress_level,
-    // };
-    // console.log('taskToAdd: ', taskToAdd);
-    // console.log('taskToAdd2: ', taskToAdd2);
+
     console.log('Value of form before post: ', form);
     console.log(JSON.stringify(form));
-    // console.log(
-    //   'Stringified value of form before post: ',
-    //   JSON.stringify(form)
-    // );
 
     return this.http.post('http://localhost:3000/tasks', form, options);
   }
