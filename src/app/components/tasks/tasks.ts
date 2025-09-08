1e3226ff1bbe0de1db71af8f4b7f7727af9b498a
ScottCatton <78232367+MapleSerup@users.noreply.github.com>
2025-09-08 17:48:04 -0400

    Added one-way sort buttons for reqs

--- src/app/components/tasks/tasks.ts
+++ src/app/components/tasks/tasks.ts
@@ -25,7 +25,29 @@ export class Tasks implements OnInit {
       this.tasks = this.tasks.filter((t) => t.id !== taskId);
     });
   }
+  sortByTitle(): void {
+    this.tasks = [...this.tasks].sort((a, b) =>
+      a.title.localeCompare(b.title.toString())
+    );
+  }
+  sortByPriority(): void {
+    const priorityOrder: { [key: string]: number } = {
+      low: 1,
+      medium: 2,
+      high: 3,
+    };
 
+    this.tasks = [...this.tasks].sort(
+      (a, b) =>
+        priorityOrder[a.priority_level.toString()] -
+        priorityOrder[b.priority_level.toString()]
+    );
+  }
+  sortByDueDate(): void {
+    this.tasks = [...this.tasks].sort((a, b) =>
+      a.time.localeCompare(b.time.toString())
+    );
+  }
   ngOnInit() {
     this.service.getTasks().subscribe(
       (res: any) => {
